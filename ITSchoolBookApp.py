94345d557579604b155330f1282308350c7a6982
Alexandru <alexandru.todireanu93@gmail.com>
2021-05-24 17:21:48 +0300

    Implemented new feature that will delete only one selected book -> delete book
    Used try->except->else to make sure that there are no errors while running this feature.
    
    Linked the new list books feature this way the "empty file" message will be printed when the delete book feature is selected and the file is empty.

--- ITSchoolBookApp.py
+++ ITSchoolBookApp.py
@@ -382,6 +382,41 @@ def share_book():
         print(f"Book has been shared with {share_with} successfully")
 
 
+# delete book function
+def delete_book():
+    if list_books() is False:
+        return
+    updated_list =[]
+    book_found = []
+    delete_book = input("Which book would you like to delete? -> ")
+    try:
+        with open("booksDB.csv", mode='r', newline='') as readFile:
+            reader = csv.DictReader(readFile, fieldnames=fieldnames, delimiter=',')
+            for row in reader:
+                updated_list.append(row)
+                if row[fieldnames[0]] == delete_book.title():
+                    book_found.append(True)
+                    updated_list.remove(row)
+                    print("Book deleted")
+                else:
+                    book_found.append(False)
+        readFile.close()
+
+        if True not in book_found:
+            print("Book not found, returning to main menu...")
+            print()
+            return
+
+        with open("booksDB.csv", mode='w', newline='') as writeFile:
+            writer = csv.DictWriter(writeFile, fieldnames=fieldnames)
+            writer.writerows(updated_list)
+        writeFile.close()
+    except IOError:
+        print("Error updating file")
+    else:
+        print()
+
+
 # Main Menu function
 def main_menu():
     options = ("Add a book", "List the existing books", "Update a book", "Share a book", "Leave a note", "Clear file", "Delete book", "Quit")
